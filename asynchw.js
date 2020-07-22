3975620356dab8754e9b950f5022c2754466416e
Safiza <safiza@utem.edu.my>
2020-07-22 10:27:17 +0800

    second commit

--- asynchw.js
+++ asynchw.js
@@ -7,13 +7,23 @@
 var fs = require('fs')
 
 console.log("Begin reading file");
-var data = fs.readFileSync("input.txt");//read a file
-console.log(data.toString());//get the response
-console.log("End reading file")
-console.log("Begin appending file")
-fs.appendFileSync("input.txt","I am appending a file")
-console.log("End appending file")
-console.log("Begin re-reading file")
-var anotherData = fs.readFileSync("input.txt")
-console.log(anotherData.toString());
-console.log("Finish re-reading file")
+fs.readFile("input.txt",(err, data)=>{
+	if(err) console.log(err)
+	console.log(data.toString());
+	console.log("End reading file");
+	console.log("Appending something");
+	fs.appendFileSync("input.txt","I am appending something",(err)=>{
+		if(err) console.log(err);
+		console.log("Succesfully appending something")
+		console.log("Begin re-reading file")
+	///////////////////////////
+		fs.readFile("input.txt",(err, data)=>{
+			if(err) console.log(err)
+			console.log(data.toString());
+			console.log("Finish re-reading file");
+		})	
+	})
+})
+
+
+
